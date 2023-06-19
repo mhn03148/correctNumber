58ed305d8620c134a6ad0a3cc8903a9bddcac231
jujinhyeong <mhn03148@naver.com>
2023-06-19 13:13:32 +0900

    Initial Commit

---
+++ correctNumber/correctNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  correctNumberApp.swift
+//  correctNumber
+//
+//  Created by 주진형 on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct correctNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
